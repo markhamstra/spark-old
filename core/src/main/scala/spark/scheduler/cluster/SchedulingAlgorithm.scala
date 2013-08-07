8a4d5fd0f0ca2ef42a3294582d56ceb45c78c4b7
Mark Hamstra <markhamstra@gmail.com>
2013-08-06 17:32:09 -0700

    wip

--- core/src/main/scala/spark/scheduler/cluster/SchedulingAlgorithm.scala
+++ core/src/main/scala/spark/scheduler/cluster/SchedulingAlgorithm.scala
@@ -30,17 +30,9 @@ private[spark] class FIFOSchedulingAlgorithm extends SchedulingAlgorithm {
   override def comparator(s1: Schedulable, s2: Schedulable): Boolean = {
     val priority1 = s1.priority
     val priority2 = s2.priority
-    var res = math.signum(priority1 - priority2)
-    if (res == 0) {
-      val stageId1 = s1.stageId
-      val stageId2 = s2.stageId
-      res = math.signum(stageId1 - stageId2)
-    }
-    if (res < 0) {
-      return true
-    } else {
-      return false
-    }
+    val schedSignum = math.signum(priority1 - priority2)
+    if (schedSignum != 0) schedSignum < 0
+    else math.signum(s1.stageId - s2.stageId) < 0  // stageIds must be distinct
   }
 }
 
@@ -52,30 +44,17 @@ private[spark] class FairSchedulingAlgorithm extends SchedulingAlgorithm {
     val runningTasks2 = s2.runningTasks
     val s1Needy = runningTasks1 < minShare1
     val s2Needy = runningTasks2 < minShare2
-    val minShareRatio1 = runningTasks1.toDouble / math.max(minShare1, 1.0).toDouble
-    val minShareRatio2 = runningTasks2.toDouble / math.max(minShare2, 1.0).toDouble
+    val minShareRatio1 = runningTasks1.toDouble / math.max(minShare1, 1.0)
+    val minShareRatio2 = runningTasks2.toDouble / math.max(minShare2, 1.0)
     val taskToWeightRatio1 = runningTasks1.toDouble / s1.weight.toDouble
     val taskToWeightRatio2 = runningTasks2.toDouble / s2.weight.toDouble
-    var res:Boolean = true
-    var compare:Int = 0
 
-    if (s1Needy && !s2Needy) {
-      return true
-    } else if (!s1Needy && s2Needy) {
-      return false
-    } else if (s1Needy && s2Needy) {
-      compare = minShareRatio1.compareTo(minShareRatio2)
-    } else {
-      compare = taskToWeightRatio1.compareTo(taskToWeightRatio2)
-    }
-
-    if (compare < 0) {
-      return true
-    } else if (compare > 0) {
-      return false
-    } else {
-      return s1.name < s2.name
-    }
+    if (s1Needy == s2Needy) {
+      val compare = if (s1Needy) minShareRatio1.compareTo(minShareRatio2)
+                    else taskToWeightRatio1.compareTo(taskToWeightRatio2)
+      if (compare == 0) s1.name < s2.name
+      else compare < 0
+    } else s1Needy && !s2Needy
   }
 }
 
